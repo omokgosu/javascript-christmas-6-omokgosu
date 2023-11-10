561373bec46ce9177d3dc1c7c029ef92225ffd00
양진성 <anfakt0606@naver.com>
2023-11-10 12:03:35 +0900

    [ fix ] 4차 추가공지대로 파일수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,10 @@
-export default OutputView = {
+OutputView.js
+const OutputView = {
     printMenu() {
         Console.print("<주문 메뉴>");
         // ...
     }
     // ...
 }
+
+export default OutputView;
