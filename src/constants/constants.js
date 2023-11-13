a7f603b99cc3be0837d0b23015cec441b4302610
양진성 <anfakt0606@naver.com>
2023-11-13 18:41:53 +0900

    [ fix ] 띄어쓰기 , 오타로 인한 테스트 케이스 실패 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -5,13 +5,13 @@ export const USER_INPUT = {
 
 export const MESSAGE = {
   PREVIEW: (date) => `12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리보기!`,
-  ORDER_MENU: '\n<주문메뉴>',
+  ORDER_MENU: '\n<주문 메뉴>',
   TOTAL_PRICE: '\n<할인 전 총주문 금액>',
   SERVICE_MENU: '\n<증정 메뉴>',
   BENEFITS_DETAILS: '\n<혜택 내역>',
   BENEFITS_PRCIE: '\n<총혜택 금액>',
   SALE_PRICE: '\n<할인 후 예상 결제 금액>',
-  BADGE: '\n<12월 이벤트 뱃지>',
+  BADGE: '\n<12월 이벤트 배지>',
   NO: '없음'
 }
 
